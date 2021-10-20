a8d78d7c5e5249249926b5943d110d968b5adb62
gaoyangcheng <630162918@qq.com>
2021-10-20 21:50:14 +0800

    commit at 21/10/20

--- lib/driver/driver.hpp
+++ lib/driver/driver.hpp
@@ -29,6 +29,7 @@ private:
     int bus = 1; // bus 1 (pin 3 and pin 5)
     int address = 0x40;
     int frequency = 50; // default 50 Hz
+    
     // Calibrated for SG-90 servo (top of the pan-tilt) and MG-90S servo (bottom of the pan-tilt)
     int servoMin = 112; // 150 for the SG-90 on the top of the pan-tilt
     int servoMax = 484; // 336 for the SG-90 on the top of the pan-tilt
